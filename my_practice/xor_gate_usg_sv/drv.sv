ea8dd71100ea4059b6e53babbf68886605dc51c7
swathi bawge <swathiveda4632@gmail.com>
2026-04-09 23:10:28 +0530

    Update drv.sv

--- my_practice/xor_gate_usg_sv/drv.sv
+++ my_practice/xor_gate_usg_sv/drv.sv
@@ -2,19 +2,19 @@ class driver;
 	mailbox #(transaction) mbx;
 	virtual and_if vif;
 
-  function new(mailbox #(transaction) mbx,virtual and_if vif);
-	this.mbx=mbx;
-	this.vif=vif;
-endfunction
+ 	function new(mailbox #(transaction) mbx,virtual and_if vif);
+		this.mbx=mbx;
+		this.vif=vif;
+	endfunction
 
-task run();
-	transaction tx;
-	forever begin
-		mbx.get(tx);
-		vif.a<=tx.a;
-		vif.b<=tx.b;
-		#10;
-	end
-endtask
+	task run();
+		transaction tx;
+			forever begin
+				mbx.get(tx);
+				vif.a<=tx.a;
+				vif.b<=tx.b;
+				#10;
+			end
+	endtask
 endclass
 
