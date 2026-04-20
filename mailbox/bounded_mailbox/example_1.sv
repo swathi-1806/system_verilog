6f508d418596ce75b7819ea5c8733ae3ba530628
swathi bawge <swathiveda4632@gmail.com>
2026-04-20 23:15:06 +0530

    Update example_1.sv

--- mailbox/bounded_mailbox/example_1.sv
+++ mailbox/bounded_mailbox/example_1.sv
@@ -4,11 +4,11 @@ module tb;
 
   mailbox mbox=new(2);
   	initial begin
-      value=10;
-    pi =3.14;
+      
   //putting data into the mailbox    
       mbox.put(10);
       mbox.put(3.14);
+  //the mailbox is full as it is bounded mailbox of 2 so we can't put more data into the mailbox
       
   //getting values from the mailbox
       mbox.get(value);//get 1st item from mailbox nd convert it into int & store it in the value
