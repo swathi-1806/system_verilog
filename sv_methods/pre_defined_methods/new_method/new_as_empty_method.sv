f282ba3a11e77d528289191bf666e5f99008a302
swathi bawge <swathiveda4632@gmail.com>
2026-05-23 19:07:13 +0530

    Update new_as_empty_method.sv

--- sv_methods/pre_defined_methods/new_method/new_as_empty_method.sv
+++ sv_methods/pre_defined_methods/new_method/new_as_empty_method.sv
@@ -11,3 +11,41 @@ class eth_pkt;
     $display("new as a empty method");
   endfunction
   
+  function void print();
+    $display("---------------------------------------------------------------------------------------------------------------------");
+    $display("preamble=%0d",preamble);
+    $display("sof=%0d",sof);
+    $display("sa=%0d",sa);
+    $display("da=%0d",da);
+    $display("len=%0d",len);
+    $display("crc=%0d",crc);
+    $display("payload=%p",payload);
+  endfunction
+  
+  constraint a_c{
+    len inside {[10:25]};
+    payload.size()==len;
+  }
+  
+endclass
+
+module tb;
+  eth_pkt pkt;
+  initial begin
+    pkt=new();
+    pkt.randomize();
+    pkt.print();
+  end
+endmodule
+
+/*
+# new as a empty method
+# ---------------------------------------------------------------------------------------------------------------------
+# preamble=14906476018882073
+# sof=251
+# sa=210838264658979
+# da=197270863703325
+# len=19
+# crc=0
+# payload='{-51, -99, -18, -71, -68, -58, 20, 120, 111, 37, 31, -124, -1, 6, 115, 84, -91, 42, 118}
+*/
