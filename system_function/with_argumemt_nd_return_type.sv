b23acef42327bb7fe725633e896ebbecda9704c4
swathi bawge <swathiveda4632@gmail.com>
2026-05-23 16:42:27 +0530

    Create with_argumemt_nd_return_type.sv

---
+++ system_function/with_argumemt_nd_return_type.sv
@@ -0,0 +1,31 @@
+class sample;
+  rand bit [3:0]a;
+  rand bit [3:0]b;
+  bit [4:0]c;
+  function int add(input int p,q);
+    //add=p+q;
+    return p+q;
+  endfunction
+endclass
+
+module tb;
+  sample s;
+  initial begin 
+    repeat(5)begin
+    s= new();
+    s.randomize();
+    s.c = s.add(s.a,s.b);
+      $display("[%0d]+[%0d]=[%0d]",s.a,s.b,s.c);
+    end
+  end
+endmodule
+
+/*
+output
+# [3]+[4]=[7]
+# [5]+[7]=[12]
+# [3]+[2]=[5]
+# [11]+[15]=[26]
+# [4]+[4]=[8]
+*/
+    
