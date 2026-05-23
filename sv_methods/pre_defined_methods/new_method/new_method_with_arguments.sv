91530dbdd734181a0b25cb0720db21ffd38528d5
swathi bawge <swathiveda4632@gmail.com>
2026-05-23 19:19:43 +0530

    Create new_method_with_arguments.sv

---
+++ sv_methods/pre_defined_methods/new_method/new_method_with_arguments.sv
@@ -0,0 +1,58 @@
+class eth_pkt;
+  rand bit [55:0]preamble;
+  rand bit [7:0]sof;
+  rand bit [47:0]sa;
+  rand bit [47:0]da;
+  rand bit [15:0]len;
+       bit [31:0]crc; 
+  rand byte payload[$];
+
+  function new(input bit[55:0]a,bit[7:0]b,bit[47:0]c,bit [47:0]d,[15:0]e);
+    $display("============with arguments===========");
+    preamble = a;
+    sof = b;
+    sa = c;
+    da = d;
+    len = e;
+  endfunction
+  
+  function void print();
+    $display("---------------------------------------------------------------------------------------------------------------------");
+    $display("preamble=%0d",preamble);
+    $display("sof=%0d",sof);
+    $display("sa=%0d",sa);
+    $display("da=%0d",da);
+    $display("len=%0d",len);
+    $display("crc=%0d",crc);
+    $display("payload=%p",payload);
+  endfunction
+  
+  constraint a_c{
+    len inside {[10:25]};
+    payload.size()==len;
+  }
+  
+endclass
+
+module tb;
+  eth_pkt pkt;
+  initial begin
+    pkt=new(500,100,400,400,15);
+   
+    //pkt.randomize();
+    pkt.print();
+  end
+endmodule
+  
+
+/*
+# ============with arguments===========
+# ---------------------------------------------------------------------------------------------------------------------
+# preamble=500
+# sof=100
+# sa=400
+# da=400
+# len=15
+# crc=0
+# payload='{}
+*/
