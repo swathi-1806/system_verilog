4513a9ba50d5c48b49ff8841decd3c6f12ba070f
swathi bawge <swathiveda4632@gmail.com>
2026-05-26 22:59:03 +0530

    Create nested_class.sv

---
+++ classess_in_sv/nested_class.sv
@@ -0,0 +1,49 @@
+class bank;
+
+  string bank_name;
+
+  // Nested class
+  class account;
+
+    string acc_holder;
+    int balance;
+
+    function void display();
+      $display("Account Holder = %s", acc_holder);
+      $display("Balance        = %0d", balance);
+    endfunction
+
+  endclass
+
+  function void show_bank();
+    $display("Bank Name = %s", bank_name);
+  endfunction
+
+endclass
+
+
+
+module tb;
+
+  bank b_h;
+  bank::account a_h;
+
+  initial begin
+
+    // Bank object
+    b_h = new();
+    b_h.bank_name = "SBI";
+
+    // Account object
+    a_h = new();
+    a_h.acc_holder = "Swathi";
+    a_h.balance = 50000;
+
+    // Display
+    b_h.show_bank();
+
+    a_h.display();
+
+  end
+
+endmodule
