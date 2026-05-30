0e8fd5ca4575aa255f533cf82482934441fd6a33
swathi bawge <swathiveda4632@gmail.com>
2026-05-30 20:39:41 +0530

    Update example1.sv

--- user_defined_data_types/enum/example1.sv
+++ user_defined_data_types/enum/example1.sv
@@ -1,1 +1,40 @@
+module traffic_light;
 
+  // Enum declaration
+  typedef enum {
+    RED,
+    YELLOW,
+    GREEN
+  } light_t;
+
+  light_t current_light;
+
+  initial begin
+
+    current_light = RED;
+    $display("Current Light = %s", current_light.name());
+
+    #10;
+    current_light = GREEN;
+    $display("Current Light = %s", current_light.name());
+
+    #10;
+    current_light = YELLOW;
+    $display("Current Light = %s", current_light.name());
+
+    #10;
+    current_light = RED;
+    $display("Current Light = %s", current_light.name());
+
+  end
+
+endmodule
+
+/*
+output:
+
+# Current Light = RED
+# Current Light = GREEN
+# Current Light = YELLOW
+# Current Light = RED
+*/
