d28fc90fbd2bd2d7b43c3f90a711f278527ee74b
swathi bawge <swathiveda4632@gmail.com>
2026-05-30 20:43:42 +0530

    Update example1.sv

--- user_defined_data_types/struct/example1.sv
+++ user_defined_data_types/struct/example1.sv
@@ -1,1 +1,26 @@
+module tb;
 
+  typedef struct {
+    string name;
+    int age;
+    int marks;
+  } student_t;
+
+  student_t s1;
+
+  initial begin
+    s1.name  = "Swathi";
+    s1.age   = 20;
+    s1.marks = 95;
+
+    $display("Name  = %s", s1.name);
+    $display("Age   = %0d", s1.age);
+    $display("Marks = %0d", s1.marks);
+  end
+
+endmodule
+/*
+# Name  = Swathi
+# Age   = 20
+# Marks = 95
+*/
