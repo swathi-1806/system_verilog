ad04171af527c84e0abfdf71aa1eed29cd3b7749
swathi-1806 <swathiveda4632@gmail.com>
2026-06-30 21:24:35 +0530

    constraints practiced

--- constraints/multiple_constraints.sv
+++ constraints/multiple_constraints.sv
@@ -23,12 +23,51 @@ module tb;
   initial begin
     p = new();
     repeat(10) begin
-      if(p.randomize())
+      if(p.randomize())begin
         $display("a = %0d", p.a);
-        $display("b= %0d", p.b);   
-      else
+        $display("b= %0d", p.b);
+		$display("--------------");
+		end
+      else begin
         $display("Randomization Failed");
+      end
     end
   end
 
 endmodule
+
+/*
+output:
+# a = 62
+# b= 1313
+# --------------
+# a = 98
+# b= 1378
+# --------------
+# a = 78
+# b= 1134
+# --------------
+# a = 56
+# b= 1048
+# --------------
+# a = 74
+# b= 1266
+# --------------
+# a = 88
+# b= 1165
+# --------------
+# a = 67
+# b= 1397
+# --------------
+# a = 76
+# b= 1331
+# --------------
+# a = 82
+# b= 1331
+# --------------
+# a = 58
+# b= 1426
+# --------------
+
+*/
+
